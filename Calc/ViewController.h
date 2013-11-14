29c9bd261ee183b8c8b369dd5d54df06add6354a
t2yomlt <2011531014@ma244.is.env.kitakyu-u.ac.jp>
2013-11-14 16:07:12 +0900

    Calcuration

--- Calc/ViewController.h
+++ Calc/ViewController.h
@@ -8,6 +8,13 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController{
+
+
+    
+    __weak IBOutlet UILabel *ResultLabel;
+    
+}
+- (IBAction)One:(id)sender;
 
 @end
